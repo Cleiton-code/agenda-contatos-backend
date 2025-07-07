c8b9d74fe26508f1171212996c6a28b2c0f342d7
Cleiton valentim <cleiton_valentim@hotmail.com.br>
2025-07-07 12:29:10 -0300

    First commit

---
+++ src/main/java/com/fatec/agenda_contatos/GerenciadorDeContatosApplication.java
@@ -0,0 +1,13 @@
+package com.fatec.agenda_contatos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorDeContatosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorDeContatosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fatec/agenda_contatos/GerenciadorDeContatosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatec.agenda_contatos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorDeContatosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
